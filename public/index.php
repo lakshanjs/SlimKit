81d434dce5661b04d34f3355c34a71ff0867a9b1
Lakshan Jayasinghe <lakshan.jayasinghe@gmail.com>
2025-08-15 17:20:11 +0530

    update

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 declare(strict_types=1);
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $app = require __DIR__ . '/../app/bootstrap.php';
 $app->run();
